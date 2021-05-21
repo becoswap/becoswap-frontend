07cc0602efbaf222e15f38d63280a434277909ec
becoswap-dev <83570067+becoswap-dev@users.noreply.github.com>
2021-05-21 17:14:40 +0700

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -7,12 +7,12 @@ const pools: PoolConfig[] = [
     stakingToken: tokens.beco,
     earningToken: tokens.beco,
     contractAddress: {
-      97: '0xd3af5fe61dbaf8f73149bfcfa9fb653ff096029a',
-      56: '0x8fe4D28476cDd43D36a12EB47dC3243C1925f263',
+       97: '0x1d32c2945C8FDCBc7156c553B7cEa4325a17f4f9',
+       56: '0x2aE8E3166fF75aF8aBB9bea2D453F084cBcd8FDA',
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
-    tokenPerBlock: '10',
+    tokenPerBlock: '100',
     sortOrder: 1,
     isFinished: false,
   }
