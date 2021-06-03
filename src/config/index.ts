06448f9f5df61f9505e3341482124600457c4118
becoswap-dev <cool@becoswap.com>
2021-06-03 21:47:32 +0700

    update beco per block

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,14 +9,9 @@ BigNumber.config({
 export const BSC_BLOCK_TIME = 3
 
 // BECO_PER_BLOCK details
-// 40 BECO is minted per block
-// 18 BECO per block is sent to Burn pool (A farm just for burning cake)
-// 10 BECO per block goes to BECO syrup pool
-// 12 BECO per block goes to Yield farms and lottery
-// BECO_PER_BLOCK in config/index.ts = 40 as we only change the amount sent to the burn pool which is effectively a farm.
-// BECO/Block in components/CakeStats.tsx = 22 (40 - Amount sent to burn pool)
+// 70 BECO is minted per block
 
-export const BECO_PER_BLOCK = new BigNumber(100)
+export const BECO_PER_BLOCK = new BigNumber(70)
 export const BLOCKS_PER_YEAR = new BigNumber((60 / BSC_BLOCK_TIME) * 60 * 24 * 365) // 10512000
 export const BASE_URL = 'https://becoswap.com'
 export const BASE_EXCHANGE_URL = 'https://exchange.becoswap.com'
--- src/views/Home/components/CakeStats.tsx
+++ src/views/Home/components/CakeStats.tsx
@@ -42,7 +42,7 @@ const CakeStats = () => {
         </Row>
         <Row>
           <Text fontSize="14px">{t('New BECO/block')}</Text>
-          <CardValue fontSize="14px" decimals={0} value={100} />
+          <CardValue fontSize="14px" decimals={0} value={70} />
         </Row>
         <Row>
           <Text fontSize="14px">{t('Transfer BECO Fee')}</Text>
