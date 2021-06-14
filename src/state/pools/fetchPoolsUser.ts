3b7805932defc5374edab3d1c722cc368a62a73c
becoswap-dev <cool@becoswap.com>
2021-06-14 21:41:40 +0700

    update

--- src/state/pools/fetchPoolsUser.ts
+++ src/state/pools/fetchPoolsUser.ts
@@ -6,7 +6,6 @@ import multicall from 'utils/multicall'
 import { getAddress, getMasterChefAddress } from 'utils/addressHelpers'
 import { getWeb3NoAccount } from 'utils/web3'
 import BigNumber from 'bignumber.js'
-import { TRANSFER_TAX } from 'config'
 
 // Pool 0, Cake / Cake is a different kind of contract (master chef)
 // BNB pools use the native BNB token (wrapping ? unwrapping is done at the contract level)
@@ -95,7 +94,7 @@ export const fetchUserPendingRewards = async (account) => {
 
   // Beco / Beco pool
   const pendingReward = await masterChefContract.methods.pendingBeco('0', account).call()
-  //const pendingRewardAfterTax = pendingReward - pendingReward/TRANSFER_TAX;
+  // const pendingRewardAfterTax = pendingReward - pendingReward/TRANSFER_TAX;
 
   return { 0: new BigNumber(pendingReward).toJSON() }
 }
--- src/views/Home/components/CakeStats.tsx
+++ src/views/Home/components/CakeStats.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TRANSFER_TAX } from 'config'
+import { BECO_PER_BLOCK, TRANSFER_TAX } from 'config'
 
 import { Card, CardBody, Heading, Text } from '@becoswap-libs/uikit'
 import styled from 'styled-components'
@@ -27,6 +27,7 @@ const CakeStats = () => {
   const totalSupply = useTotalSupply()
   const burnedBalance = getBalanceNumber(useBurnedBalance(getCakeAddress()))
   const cakeSupply = totalSupply ? getBalanceNumber(totalSupply) - burnedBalance : 0
+  const becoPerBlock = BECO_PER_BLOCK.toNumber()
   return (
     <StyledCakeStats>
       <CardBody>
@@ -43,7 +44,7 @@ const CakeStats = () => {
         </Row>
         <Row>
           <Text fontSize="14px">{t('New BECO/block')}</Text>
-          <CardValue fontSize="14px" decimals={0} value={70} />
+          <CardValue fontSize="14px" decimals={0} value={becoPerBlock} />
         </Row>
         <Row>
           <Text fontSize="14px">{t('Transfer BECO Fee')}</Text>
